b47f9270dc2e2416d27978295e1d2f3339fc323c
Gabriel Dominic A. Miranda <gamiranda@student.hau.edu.ph>
2025-10-18 15:45:36 +0800

    Docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,14 @@ import todoRoutes from "./routes/todoRoutes";
 
 const app = express();
 
-// Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+}));
+
 app.use(express.json());
 
-// Routes
 app.use("/api/todos", todoRoutes);
 
 export default app;
--- frontend/src/screens/TodoScreen.jsx
+++ frontend/src/screens/TodoScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:4000/api/todos"; 
+// const API_URL = "http://localhost:4000/api/todos"; 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api/todos"; // Docker
 
 // --- API Functions Section ---
 
